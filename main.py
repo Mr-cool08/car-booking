3116b2df3dc2d3125aea967032d707247e814850
Liam Skoldator <liam@suorsa.se>
2025-04-11 16:27:48 +0200

    turned off debug mode

--- main.py
+++ main.py
@@ -298,4 +298,4 @@ if __name__ == '__main__':
     global conn
     conn = sqlitecloud.connect(DATABASE_LOGIN)
     init_db()
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=False, host="0.0.0.0", port=80)
